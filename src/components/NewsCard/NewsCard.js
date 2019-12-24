f311aa048372ae24b510ef9e2515b5c90833a5b4
Radhika Murugesan <radhika.murugesan@perficient.com>
2019-12-24 17:10:43 +0530

    Design changes

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import moment from 'moment';
 import { Card, Icon, Image } from 'semantic-ui-react';
+import LinesEllipsis from 'react-lines-ellipsis'
 import './style.css';
 
-
+const {limit} = 250;
 
 function NewsCard(props) {
     console.log('property', props);
     return (
-    // <div className="card">
-    //                 <img className="card-img-top" src={props.newsCardData.urlToImage} />
-
-    //     <div className="container">
-    //                 <h4><b>{props.newsCardData.source.name}</b></h4>
-    //                 <h5><b>{props.newsCardData.title}</b></h5>
-    //                 <p>{props.newsCardData.description}</p>
-    //                 <p><b>{moment(props.newsCardData.publishedAt).format("MMM Do YY")}</b></p>
-    //             </div>
-    // </div>
+    
 
 <Card>
 <Image src={props.newsCardData.urlToImage} wrapped ui={false} />
 <Card.Content>
-  <Card.Header>{props.newsCardData.source.name}</Card.Header>
-  <Card.Meta>{props.newsCardData.title}</Card.Meta>
+ <Card.Header>
+      <LinesEllipsis
+  text={props.newsCardData.title}
+  maxLine='2'
+  ellipsis='...'
+  trimRight
+  basedOn='letters'
+  style={{ whiteSpace: 'pre-wrap' }}
+/>
+</Card.Header>
   <Card.Description>
   {props.newsCardData.description}
   </Card.Description>
